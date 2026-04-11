8bbd4a16e999502aa3cdefde166a204d6da029a2
goropikari <goropikari56@gmail.com>
2026-04-11 23:17:52 +0900

    add devcontainer

--- dev/init.lua
+++ dev/init.lua
@@ -9,6 +9,16 @@ vim.opt.rtp:prepend(root)
 vim.opt.rtp:append(dev_dir .. "/snacks.nvim")
 vim.opt.rtp:append(dev_dir .. "/terminals.nvim")
 
+-- terminal mode を escape で抜ける
+vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]])
+
+-- terminal mode で esc を terminal で開いているアプリに渡す
+vim.keymap.set("t", "<C-q>", function()
+  vim.fn.chansend(vim.b.terminal_job_id, "\x1b")
+end)
+
+vim.o.statusline = "%=%r %{%v:lua.require('claude').statusline_summary()%} %y"
+
 require("snacks").setup({})
 require("terminals").setup({}) -- <C-t> to toggle
 
